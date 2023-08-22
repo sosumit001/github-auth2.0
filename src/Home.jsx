5680f71031dde8ea605c05dd83102b3d064354ca
sosumit001 <sosumit001@gmail.com>
2023-08-22 11:18:02 +0530

    ..

---
+++ src/Home.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect} from "react"
+import { getUserData } from "./callback/callback"
+import UserInformation from "./UserInformation"
+function Home() {
+
+  const [user, setUser] = useState(null)
+
+  const handleLogout = () => {
+    localStorage.removeItem('access_token')
+    setUser(null)
+  }
+    
+  useEffect(() => {
+    const token = localStorage.getItem('access_token')
+
+    if(token !== null & user === null) {
+      async function fetchUserData() {
+        const data = await getUserData()
+        setUser(data)
+      }
+      
+    fetchUserData()
+    }
+
+    
+  }, [user])
+  const handleLogin = () => {
+      window.location.href = 'https://github.com/login/oauth/authorize?client_id=...&scope=user'
+  }
+  
+  return (
+      <>
+      {
+        user === null ? 
+        <button onClick={handleLogin}>Log in With Github</button>
+        : 
+        <UserInformation userData={user} handleLogout={handleLogout} />
+      }
+      </>
+    )
+  }
+  
+  export default Home
+  
